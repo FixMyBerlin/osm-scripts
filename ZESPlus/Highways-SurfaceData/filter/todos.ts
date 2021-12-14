357c88f42e3440e113c4a13eb6edcd13bcc53f7c
Tobias <tobias@fixmycity.de>
2021-12-14 16:41:00 +0100

    Surface: Remove irrelevant ways from TODO lists

--- ZESPlus/Highways-SurfaceData/filter/todos.ts
+++ ZESPlus/Highways-SurfaceData/filter/todos.ts
@@ -1,13 +1,20 @@
 import { Feature } from "../../utils/types"
+import { irrelevanteWege } from "./irrelevanteWege"
 
 export const TODO_WegeOhneSurface = (feature: Feature) => {
+  if (irrelevanteWege(feature)) return false
+
   return !feature.properties.surface
 }
 export const TODO_WegeOhneSmoothnessAberMitSurface = (feature: Feature) => {
+  if (irrelevanteWege(feature)) return false
+
   return feature.properties.surface && !feature.properties.smoothness
 }
 
 // A list of borken smoothness values that we need to cleanup in OSM.
 export const TODO_fixSmoothnessValues = (feature: Feature) => {
+  if (irrelevanteWege(feature)) return false
+
   return ["verbad"].includes(feature.properties.smoothness)
 }
