89a375c4e61153f7793b80cdde873e62051b2fbd
Tobias <tobias@fixmycity.de>
2021-12-22 16:33:38 +0100

    HighwayType: Improve todo filter

--- ZESPlus/Highways-HighwayTypeData/filter/todos.ts
+++ ZESPlus/Highways-HighwayTypeData/filter/todos.ts
@@ -1,10 +1,15 @@
-// TODO: Wie wollen wir Bürgersteige mit Schild DE:239,1022-10 mappen?, https://www.openstreetmap.org/way/883876171
-// Auch als sidewalk oder nur als footway?
+// Hinweis: Bürgersteige mit Schild DE:239,1022-10 https://www.openstreetmap.org/way/883876171
+// werden nicht als "sidewalk" gemapped.
 export const TODO_BuergersteigTaggingFehlt = (feature) => {
   return (
     feature.properties.highway === "footway" &&
     feature.properties.is_sidepath === "yes" &&
-    (!feature.properties.footway || feature.properties.footway !== "sidewalk")
+    (!feature.properties.footway ||
+      feature.properties.footway !== "sidewalk") &&
+    // If bikes are allowed, we don't tag 'sidewalk'
+    !["yes", "designated"].includes(feature.properties.bicycle) &&
+    // Crossings are not tagged as 'sidewalk'
+    !feature.properties.crossing
   )
 }
 
