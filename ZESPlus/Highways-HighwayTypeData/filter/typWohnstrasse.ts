71294fc60e3e0029c438b48cfb6cad45eb37dcab
Tobias <tobias@fixmycity.de>
2021-12-22 16:36:33 +0100

    HighwayType: WIP rework how typWohnstrassen works
    This becomes our default type for all we did not match more precisely.

--- ZESPlus/Highways-HighwayTypeData/filter/typWohnstrasse.ts
+++ ZESPlus/Highways-HighwayTypeData/filter/typWohnstrasse.ts
@@ -1,43 +1,49 @@
 import { irrelevanteWege } from "./irrelevanteWege"
+import { typHauptUndSammelstrasse } from "./typHauptUndSammelstrasse"
 
 // TYP A + B – Wohnweg/Wohnstraße (Erschließungsstraßen)
 // Mindset: Wir müssen hier präzise sein, um eine Abgrenung zu TypHauptUndSammelstrasse zu erreichen
 export const typWohnstrasse = (feature) => {
-  const includeWohnstrassenMitGeringerGeschwindigkeit =
-    ["residential", "unclassified"].includes(feature.properties.highway) &&
-    feature.properties.maxspeed <= 30
+  if (irrelevanteWege(feature)) return false
+  if (typHauptUndSammelstrasse(feature)) return false
 
-  const includeWohnstrssenMitSchlechterOberflaeche =
-    feature.properties.highway === "residential" &&
-    ["unpaved", "ground"].includes(feature.properties.sisurfacedewalk)
+  return feature.properties.highway === "residential"
 
-  // TODO OSM: falsches Tagging "none" way/4935070
-  const includeWohnstrassenOhneGehweg =
-    feature.properties.highway === "residential" &&
-    ["none", "no"].includes(feature.properties.sidewalk)
+  // const includeWohnstrassenMitGeringerGeschwindigkeit =
+  //   ["residential", "unclassified"].includes(feature.properties.highway) &&
+  //   feature.properties.maxspeed <= 30
 
-  // kein lane-marking is indikator für Wohnstraße, ebenso lane=1
-  const includeEinspurig =
-    feature.properties.highway === "residential" &&
-    (feature.properties.lane_markings === "no" ||
-      feature.properties.lane === "1")
+  // const includeWohnstrssenMitSchlechterOberflaeche =
+  //   feature.properties.highway === "residential" &&
+  //   ["unpaved", "ground"].includes(feature.properties.sisurfacedewalk)
 
-  const includeSpielstrassen = feature.properties.highway === "living_street"
+  // // TODO OSM: falsches Tagging "none" way/4935070
+  // const includeWohnstrassenOhneGehweg =
+  //   feature.properties.highway === "residential" &&
+  //   ["none", "no"].includes(feature.properties.sidewalk)
 
-  // "Nicht" weiter klassifzierte service-straßen die Namen haben verstehen wir als Wohnstraßen
-  const includeSpezielleServiceWege =
-    feature.properties.highway === "service" &&
-    !feature.properties.service &&
-    feature.properties.name &&
-    feature.properties.tunnel !== "building_passage"
+  // // kein lane-marking is Indikator für Wohnstraße, ebenso lane=1
+  // const includeEinspurig =
+  //   feature.properties.highway === "residential" &&
+  //   (feature.properties.lane_markings === "no" ||
+  //     feature.properties.lane === "1")
 
-  return (
-    !irrelevanteWege(feature) &&
-    (includeWohnstrassenMitGeringerGeschwindigkeit ||
-      includeWohnstrssenMitSchlechterOberflaeche ||
-      includeWohnstrassenOhneGehweg ||
-      includeEinspurig ||
-      includeSpielstrassen ||
-      includeSpezielleServiceWege)
-  )
+  // const includeSpielstrassen = feature.properties.highway === "living_street"
+
+  // // "Nicht" weiter klassifzierte service-straßen die Namen haben verstehen wir als Wohnstraßen
+  // const includeSpezielleServiceWege =
+  //   feature.properties.highway === "service" &&
+  //   !feature.properties.service &&
+  //   feature.properties.name &&
+  //   feature.properties.tunnel !== "building_passage"
+
+  // return (
+  //   !irrelevanteWege(feature) &&
+  //   (includeWohnstrassenMitGeringerGeschwindigkeit ||
+  //     includeWohnstrssenMitSchlechterOberflaeche ||
+  //     includeWohnstrassenOhneGehweg ||
+  //     includeEinspurig ||
+  //     includeSpielstrassen ||
+  //     includeSpezielleServiceWege)
+  // )
 }
