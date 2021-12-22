f202bf5cab9f5dca6df003de2b45c585a6a33a74
Tobias <tobias@fixmycity.de>
2021-12-22 15:48:37 +0100

    HighwayType: Move Geschäftsstraßen note in separate file

--- ZESPlus/Highways-HighwayTypeData/filter/typAusserorts.ts
+++ ZESPlus/Highways-HighwayTypeData/filter/typAusserorts.ts
@@ -1,10 +1,5 @@
 import { irrelevanteWege } from "./irrelevanteWege"
 
-// TYP D – „Geschäftsstraße“ => Wird nicht über dieses Script gelößt.
-//
-// const Geschaeftsstraßen = (features) => {
-//   return features.properties.highway === "tertiary"
-// }
 // TYP E – Landes-/Kreisstraße [Außerorts]
 /* K 6161 ist Landstraße und führt dann in die Stadt;
 TODO: Innerhalb der Stadt müssten wir sie manuell umkategorisieren und als Hauptstraße taggen. */
