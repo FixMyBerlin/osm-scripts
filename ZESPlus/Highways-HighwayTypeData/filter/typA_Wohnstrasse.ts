718fdc2251a5fe1cbb20b484c462573b07234024
Tobias <tobias@fixmycity.de>
2022-01-06 14:11:48 +0100

    HighwayData: Remove previous code that was commented out

--- ZESPlus/Highways-HighwayTypeData/filter/typA_Wohnstrasse.ts
+++ ZESPlus/Highways-HighwayTypeData/filter/typA_Wohnstrasse.ts
@@ -25,42 +25,4 @@ export const typWohnstrasse = (feature) => {
     notDriveThrought
 
   return byType || serviceWithName || serviceWithMaxspeed
-
-  // const includeWohnstrassenMitGeringerGeschwindigkeit =
-  //   ["residential", "unclassified"].includes(feature.properties.highway) &&
-  //   feature.properties.maxspeed <= 30
-
-  // const includeWohnstrssenMitSchlechterOberflaeche =
-  //   feature.properties.highway === "residential" &&
-  //   ["unpaved", "ground"].includes(feature.properties.sisurfacedewalk)
-
-  // // TODO OSM: falsches Tagging "none" way/4935070
-  // const includeWohnstrassenOhneGehweg =
-  //   feature.properties.highway === "residential" &&
-  //   ["none", "no"].includes(feature.properties.sidewalk)
-
-  // // kein lane-marking is Indikator für Wohnstraße, ebenso lane=1
-  // const includeEinspurig =
-  //   feature.properties.highway === "residential" &&
-  //   (feature.properties.lane_markings === "no" ||
-  //     feature.properties.lane === "1")
-
-  // const includeSpielstrassen = feature.properties.highway === "living_street"
-
-  // // "Nicht" weiter klassifzierte service-straßen die Namen haben verstehen wir als Wohnstraßen
-  // const includeSpezielleServiceWege =
-  //   feature.properties.highway === "service" &&
-  //   !feature.properties.service &&
-  //   feature.properties.name &&
-  //   feature.properties.tunnel !== "building_passage"
-
-  // return (
-  //   !irrelevanteWege(feature) &&
-  //   (includeWohnstrassenMitGeringerGeschwindigkeit ||
-  //     includeWohnstrssenMitSchlechterOberflaeche ||
-  //     includeWohnstrassenOhneGehweg ||
-  //     includeEinspurig ||
-  //     includeSpielstrassen ||
-  //     includeSpezielleServiceWege)
-  // )
 }
