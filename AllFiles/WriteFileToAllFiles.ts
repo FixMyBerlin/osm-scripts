b4bfb0b154b9f0da960384a71e561de647debf00
Tobias <tobias@fixmycity.de>
2022-03-03 17:39:22 +0100

    AllFile: Only write when "npm run build:netlify"
    This makes sure we properly reset the file and not mess it up with appended links.

--- AllFiles/WriteFileToAllFiles.ts
+++ AllFiles/WriteFileToAllFiles.ts
@@ -5,13 +5,19 @@ import { allFilesFile } from "./files.const"
 export const WriteUrlToAllFilesFile = (url?: string) => {
   if (!url)
     console.error("👉 WriteUrlToAllFilesFile(): Did not receive an URL.")
+  if (process.env.WRITE_ALL_FILES_FILE !== "true") {
+    console.error(
+      "👉 WriteUrlToAllFilesFile(): Skipped. Set `WRITE_ALL_FILES_FILE=true` to enable."
+    )
+    return
+  }
 
   const string = `\n    "${url}",`
 
   fs.appendFile(allFilesFile, string, (err) => {
     if (err) throw err
     console.log(
-      "WriteUrlToAllFilesFile():",
+      "🔗 WriteUrlToAllFilesFile():",
       `Added ${url} to AllFiles.json`,
       netlifyFileUrl(allFilesFile)
     )
