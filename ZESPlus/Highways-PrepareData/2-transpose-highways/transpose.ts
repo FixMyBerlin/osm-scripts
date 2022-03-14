6d0af8dcffb5bfacd4871000031fef95a04b79f1
Tobias <tobias@fixmycity.de>
2022-03-14 13:13:59 +0100

    ZES: Prepare: Extract function in extra file

--- ZESPlus/Highways-PrepareData/2-transpose-highways/transpose.ts
+++ ZESPlus/Highways-PrepareData/2-transpose-highways/transpose.ts
@@ -1,30 +1,5 @@
-import de9im from "de9im"
-import fs from "fs"
 import { overpassComposeToGeoJson } from "../../utils/overpassToGeoJson/overpassComposeToGeoJson"
-import { AreaCallbackProps } from "../../utils/overpassToGeoJson/types"
-
-const enhanceDataWitAreaInformation: AreaCallbackProps = (areaKey, geoJson) => {
-  console.time(`⏱ enhanceData(): Ergänze FMC:Gebiete:${areaKey}=True|False`)
-
-  const file = `./ZESPlus/AreaOfInterest/output/${areaKey}.geojson`
-  // Needs to be .readFileSync or otherise all needs to become async
-  const area = JSON.parse(fs.readFileSync(file, { encoding: "utf8" }))
-
-  geoJson.features.forEach((feature) => {
-    const partOfOrOverlaps = de9im.intersects(area, feature.geometry)
-    feature.properties[`FMC:Gebiete:${areaKey}`] = `${partOfOrOverlaps}`
-    if (partOfOrOverlaps) {
-      feature.properties["FMC:Gebiete"] = [
-        areaKey,
-        feature.properties["FMC:Gebiete"],
-      ]
-        .filter((n) => n) // Remove unfefined and null values. Ruby, I miss you.
-        .join(",")
-    }
-  })
-
-  console.timeEnd(`⏱ enhanceData(): Ergänze FMC:Gebiete:${areaKey}=True|False`)
-}
+import { enhanceDataWitAreaInformation } from "./utils/enhanceDataWitAreaInformation"
 
 overpassComposeToGeoJson({
   readFile:
