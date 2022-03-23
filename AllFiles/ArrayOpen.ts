24122171406a28b08c592a68205d9cea64cbfcfc
Tobias <tobias@fixmycity.de>
2022-03-23 13:06:57 +0100

    Add "GeneratedAt" to AllFiles-Endpoint

--- AllFiles/ArrayOpen.ts
+++ AllFiles/ArrayOpen.ts
@@ -2,6 +2,7 @@ import fs from "fs"
 import { allFilesFile } from "./files.const"
 
 const open = `{
+  "GeneratedAt": "${new Date().toISOString()}",
   "AllFiles": [`
 
 fs.appendFile(allFilesFile, open, (err) => {
