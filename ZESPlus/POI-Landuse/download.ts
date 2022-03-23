a8f4d8c73f364e62b348c1d330427dc6f645cbdc
Tobias <tobias@fixmycity.de>
2022-03-23 13:00:27 +0100

    ZES: POI Landuse: More types of landuse
    Those are all landuse_s where people live or work or are present.

--- ZESPlus/POI-Landuse/download.ts
+++ ZESPlus/POI-Landuse/download.ts
@@ -5,14 +5,18 @@ import { fileName, rawOutputFolder } from "./filesFolders.const"
 const overpassQuery = `
 [out:json][timeout:25];
 (
-  nwr["landuse"="residential"](${bboxBetrachtungsraum.join(", ")});
+  nwr["landuse"="allotments"](${bboxBetrachtungsraum.join(", ")});
   nwr["landuse"="brownfield"](${bboxBetrachtungsraum.join(", ")});
+  nwr["landuse"="cemetery"](${bboxBetrachtungsraum.join(", ")});
+  nwr["landuse"="civic"](${bboxBetrachtungsraum.join(", ")});
+  nwr["landuse"="commercial"](${bboxBetrachtungsraum.join(", ")});
   nwr["landuse"="construction"](${bboxBetrachtungsraum.join(", ")});
   nwr["landuse"="farmyard"](${bboxBetrachtungsraum.join(", ")});
-  nwr["landuse"="retail"](${bboxBetrachtungsraum.join(", ")});
-  nwr["landuse"="industrial"](${bboxBetrachtungsraum.join(", ")});
-  nwr["landuse"="commercial"](${bboxBetrachtungsraum.join(", ")});
   nwr["landuse"="garages"](${bboxBetrachtungsraum.join(", ")});
+  nwr["landuse"="industrial"](${bboxBetrachtungsraum.join(", ")});
+  nwr["landuse"="religious"](${bboxBetrachtungsraum.join(", ")});
+  nwr["landuse"="residential"](${bboxBetrachtungsraum.join(", ")});
+  nwr["landuse"="retail"](${bboxBetrachtungsraum.join(", ")});
 );
 out body;>;out skel qt;
 `
