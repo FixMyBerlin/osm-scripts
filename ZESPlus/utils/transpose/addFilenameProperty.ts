f8b701d77ce98f74ac2f9ae8c1f88775962878c0
Tobias <tobias@fixmycity.de>
2022-03-28 10:39:17 +0200

    ZES POI Data: Add tippecanoe layer name to GeoJSON

--- ZESPlus/utils/transpose/addFilenameProperty.ts
+++ ZESPlus/utils/transpose/addFilenameProperty.ts
@@ -8,6 +8,10 @@ export const addFilenameProperty = (
 
   features.forEach((feature) => {
     feature.properties[`FMC:Category:${fileNamePart}`] = "true"
+    // Custom layer name for the vector tiles
+    //  Docs: https://github.com/mapbox/tippecanoe#geojson-extension
+    //  We can do this whenever there is only one category per feature (like with our POI data)
+    feature['tippecanoe'] = { layer: fileNamePart }
   })
 
   console.timeEnd("⏱ addFilenameProperty()")
