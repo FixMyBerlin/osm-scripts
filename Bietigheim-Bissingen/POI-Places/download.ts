f4789aa94fd903f0bc5b6ed58f0b187a83f62d0c
Tobias <tobias@fixmycity.de>
2022-08-01 12:26:18 +0200

    BB: Update POI places download, needs more adjustment

--- Bietigheim-Bissingen/POI-Places/download.ts
+++ Bietigheim-Bissingen/POI-Places/download.ts
@@ -2,26 +2,18 @@ import { bboxBetrachtungsraum } from "../AreaOfInterest/areas.constant"
 import { overpassDownloadJson } from "../utils/overpassDownloadJson"
 import { fileName, rawOutputFolder } from "./filesFolders.const"
 
+// TODO: Präzisieren
 const overpassQuery = `
 [out:json][timeout:25];
 (
   node
-  ["place"="village"]["name"="Zeuthen"]
+  ["place"="village"]
   (${bboxBetrachtungsraum.join(", ")});
   node
-  ["place"="village"]["name"="Eichwalde"]
+  ["place"="town"]
   (${bboxBetrachtungsraum.join(", ")});
   node
-  ["place"="village"]["name"="Schulzendorf"]
-  (${bboxBetrachtungsraum.join(", ")});
-  node
-  ["place"="town"]["name"="Königs Wusterhausen"]
-  (${bboxBetrachtungsraum.join(", ")});
-  node
-  ["place"="suburb"]["name"="Schönefeld"]
-  (${bboxBetrachtungsraum.join(", ")});
-  node
-  ["place"="suburb"]["name"="Wildau"]
+  ["place"="suburb"]
   (${bboxBetrachtungsraum.join(", ")});
 );
 out body;>;out skel qt;
