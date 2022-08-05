bc4bbd9d1f22d07d423be76992afc9494eb108e6
Tobias <tobias@fixmycity.de>
2022-08-05 16:42:21 +0200

    Landuse: Remove timout for ZES

--- utils/poiLanduse/downloadPoiLanduse.ts
+++ utils/poiLanduse/downloadPoiLanduse.ts
@@ -4,7 +4,7 @@ export const downloadPoiLanduse = ({ bboxBetrachtungsraum, outputFolder }) => {
   // For some areas, the land usage is only described with area+amenity.
   // For those, I only look at ways so I don't have to handle single nodes here.
   const overpassQuery = `
-[out:json][timeout:25];
+[out:json];
 (
   nwr["landuse"="allotments"](${bboxBetrachtungsraum.join(", ")});
   nwr["landuse"="brownfield"](${bboxBetrachtungsraum.join(", ")});
